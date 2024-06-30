e484884191ea52702385f93fa8929c6b1db9e276
Ankit Dhakal <ankitdhakal10@gmail.com>
2024-06-30 15:15:34 +0545

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
@@ -25,4 +25,9 @@ export function addToCart(productId) {
       quantity: 1
     })
   }
+}
+
+export function removeFromCart(productId) {
+
+  cart = cart.filter(cartItem => cartItem.id !== productId);
 }
