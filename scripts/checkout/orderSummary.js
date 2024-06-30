1b1884f27db8fdde03ddfa5be0186de3d03ad725
Ankit Dhakal <ankitdhakal10@gmail.com>
2024-06-30 20:43:51 +0545

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from '../../data/products.js'
 import formatCurrency from '../utils/money.js'
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 export function renderOrderSummary() {
@@ -120,9 +121,11 @@ document.querySelectorAll('.js-delete-link')
       const productId = link.dataset.productId;
 
       removeFromCart(productId);
-
+      
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
+
+      renderPaymentSummary();
     });
   });
 
@@ -134,6 +137,7 @@ document.querySelectorAll('.js-delivery-option')
       
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     })
   })
 
