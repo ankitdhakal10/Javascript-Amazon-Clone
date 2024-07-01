27515a144ca4aed846b3281b74be22979ea40896
Ankit Dhakal <ankitdhakal10@gmail.com>
2024-07-01 23:11:16 +0545

    Remove default tests and add money test

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,8 +1,8 @@
 import formatCurrency from '../scripts/utils/money.js';
 
-console.log('Test suite: ');
+console.log('test suite: formatCurrency');
 
-console.log('Convert cents into dollars: ');
+console.log('converts cents into dollars');
 
 if(formatCurrency(2095) === '20.95') {
   console.log('passed');
@@ -10,9 +10,17 @@ if(formatCurrency(2095) === '20.95') {
   console.log('failed');
 }
 
-console.log('Case for 0 cents: ');
+console.log('works with 0');
 
-if(formatCurrency(0) === '0') {
+if(formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('rounds upto the nearest cent');
+
+if(formatCurrency(2000.5) === '20.01') {
   console.log('passed');
 } else {
   console.log('failed');
