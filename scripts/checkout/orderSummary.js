bdbe14b36b6a18347c222652a3af074aa6ec664f
Ankit Dhakal <ankitdhakal10@gmail.com>
2024-07-02 13:56:40 +0545

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,5 +1,5 @@
 import { cart, removeFromCart, updateDeliveryOption } from '../../data/cart.js'
-import { products, getProduct } from '../../data/products.js'
+import { getProduct } from '../../data/products.js'
 import formatCurrency from '../utils/money.js'
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
@@ -28,7 +28,9 @@ cart.forEach((cartItem) => {
 
 
   cartSummaryHTML += `
-  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+          <div class="cart-item-container
+            js-cart-item-container
+            js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: ${dateString}
             </div>
@@ -44,14 +46,16 @@ cart.forEach((cartItem) => {
                 <div class="product-price">
                   $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity 
+                  js-product-quantity-${matchingProduct.id}">
                   <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary js-delete-link"
+                  <span class="delete-quantity-link link-primary js-delete-link
+                   js-delete-link-${matchingProduct.id}"
                    data-product-id="${matchingProduct.id}" >
                     Delete
                   </span>
@@ -107,8 +111,8 @@ data-delivery-option-id="${deliveryOption.id}"
 </div>
 `
   })
-  return html;
 
+  return html;
 }
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
@@ -142,5 +146,3 @@ document.querySelectorAll('.js-delivery-option')
   })
 
 }
-
-renderOrderSummary();
